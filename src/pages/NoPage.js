8e5196eea80f147b9c517fae2946495f2a02ea42
Shivam Harjai <harjaishivam@gmail.com>
2023-07-17 15:22:33 -0700

    final website

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,8 @@
 const NoPage = () => {
-    return <h1>404</h1>;
+    return <div>
+      <h2>404</h2>
+      <h1>Please Press the home Button in the Nav Bar</h1>
+      </div>;
 };
   
   export default NoPage;
